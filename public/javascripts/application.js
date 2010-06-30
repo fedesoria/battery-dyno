c77b9f861d23775b184244917c24fcdb41e0cf69
Federico Soria <fedesoria@gmail.com>
2010-06-29 19:00:39 -0500

    simulate dynos

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,9 +1,11 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+var dynos = 1;
 $(document).ready(function() {
   $('.add_button').click( function() {
     var temp_dyno_count = parseInt($('.dynos').html());
     $('.dynos').html(temp_dyno_count + 1);
+    dynos += 1;
     return false;
   });
 });
